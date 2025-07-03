21a20a073077de4e92c91baad089e8f00aec327e
SyedAnees21 <syedanees816@gmail.com>
2025-07-03 14:25:41 +0500

    feat: rc5 doc comments

--- rc5-rs/src/rc5.rs
+++ rc5-rs/src/rc5.rs
@@ -1,12 +1,33 @@
 use crate::{BlockCipher, Reason, Version, Word, bail};
 
+/// # RC5 control block
+///
+/// This struct represents the internal state of an RC5 cipher with a given word size.
+/// It encapsulates key expansion (`S` table), the number of rounds, and a computed version tag.
+///
+/// Parameters:
+///
+/// - `W`: The word type (e.g., `u16`, `u32`, `u64`), which must implement the [`Word`] trait.
 pub struct RC5ControlBlock<W: Word> {
+    /// RC5 parametric version
     version: Version,
+
+    /// RC5 key, which holds the raw key and its
+    /// expanded `S-Table`
     key: RC5Key<W>,
+
+    /// Defines the number of iterations during
+    /// encryption.
     rounds: usize,
 }
 
 impl<W: Word> RC5ControlBlock<W> {
+    /// RC5 constructor method
+    ///
+    /// It creates an instance of an RC5 control block from
+    /// raw key and rounds.
+    ///
+    /// Returns a result type containing the control block.
     pub fn new<K>(key: K, rounds: usize) -> Result<Self, Reason>
     where
         K: AsRef<[u8]>,
@@ -24,16 +45,20 @@ impl<W: Word> RC5ControlBlock<W> {
         })
     }
 
+    /// Returns a reference to the expanded S table used for round keys.
     #[inline]
     pub fn s_table(&self) -> &[W] {
         &self.key.s_table
     }
 
+    // Returns the configured number of RC5 rounds.
     #[inline]
     pub fn rounds(&self) -> usize {
         self.rounds
     }
 
+    /// Returns a string describing the parametric RC5 version,
+    /// e.g. `"RC5-32/12/16"`.
     #[inline]
     pub fn parametric_version(&self) -> String {
         self.version.version()
@@ -115,27 +140,35 @@ impl<W: Word> BlockCipher<W, 2> for RC5ControlBlock<W> {
 const MAX_ROUNDS: usize = 255;
 const MAX_KEY_BYTES: usize = 255;
 
+/// # RC5Key
+///
+/// Internal rc5 key  container which holds the raw key as
+/// well as expanded s-table of the raw key.
 pub struct RC5Key<W: Word> {
     raw_key: Vec<u8>,
     s_table: Vec<W>,
 }
 
 impl<W: Word> RC5Key<W> {
+    /// Creates RC5Key from raw key bytes.
+    /// Reutrns a result type containing Key or an err.
     pub fn from_raw<K>(raw: K, rounds: usize) -> Result<Self, Reason>
     where
         K: AsRef<[u8]>,
     {
         let key_bytes = raw.as_ref();
 
-        bail!(key_bytes.is_empty(), Reason::InvalidKey);
         bail!(
+            key_bytes.is_empty(),
+            Reason::InvalidKey,
             key_bytes.len() > MAX_KEY_BYTES,
             Reason::KeyTooLong {
                 current: key_bytes.len(),
                 supported: MAX_KEY_BYTES
-            }
+            },
+            rounds > MAX_ROUNDS,
+            Reason::InvalidRounds(rounds)
         );
-        bail!(rounds > MAX_ROUNDS, Reason::InvalidRounds(rounds));
 
         Ok(Self {
             s_table: expand_key::<W>(key_bytes, rounds),
@@ -148,6 +181,20 @@ impl<W: Word> RC5Key<W> {
     }
 }
 
+/// RC5 key expansion function.
+/// 
+/// Converts a user-supplied key into an expanded S-table using the RC5 mixing algorithm
+/// in little-endian byte order. This table will be used for all encryption and decryption 
+/// operations.
+/// 
+/// see more: [RC5-paper](https://www.grc.com/r&d/rc5.pdf)
+/// 
+/// # Parameters
+/// - `key`: raw key bytes.
+/// - `rounds`: number of RC5 rounds.
+///
+/// # Returns
+/// A vector containing the expanded key schedule.
 fn expand_key<W: Word>(key: &[u8], rounds: usize) -> Vec<W> {
     let word_bytes = W::BYTES;
     let key_length = key.len().max(1);
@@ -155,6 +202,8 @@ fn expand_key<W: Word>(key: &[u8], rounds: usize) -> Vec<W> {
     let expanded_length = (key_length + word_bytes - 1) / word_bytes;
     let mut key_words = vec![W::ZERO; expanded_length];
 
+    // Packing the raw bytes ino list of word-szie
+    // in a little-endian byte order
     for index in (0..key_length).rev() {
         let ix = index / word_bytes;
         key_words[ix] = key_words[ix]
@@ -167,6 +216,7 @@ fn expand_key<W: Word>(key: &[u8], rounds: usize) -> Vec<W> {
 
     s_table[0] = W::P;
 
+    // Key flattening with magic constants
     for i in 1..table_size {
         s_table[i] = s_table[i - 1].wrapping_add(W::Q);
     }
@@ -174,6 +224,8 @@ fn expand_key<W: Word>(key: &[u8], rounds: usize) -> Vec<W> {
     let (mut i, mut j) = (0, 0);
     let (mut a, mut b) = (W::ZERO, W::ZERO);
 
+    // Key mixing, this is done by interlacing key words with
+    // each other. 
     for _ in 0..(3 * table_size.max(expanded_length)) {
         a = s_table[i]
             .wrapping_add(a)
